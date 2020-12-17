99ec8d28adf779e7939b9cfa9ae2b5dfaea0b3aa
Adam <sipka.adam@gmail.com>
2020-12-17 22:24:16 +0100

    som doslova idiot

--- player.h
+++ player.h
@@ -162,8 +162,8 @@ public:
 
     // Switch to new scene after breaking 3 bottles
     bool switch_scenes(Club& club_scene) const {
-
-        if (counter == 1) {
+    
+        if (counter == 3) {
             club_scene.~Club();
             return false;
         }
